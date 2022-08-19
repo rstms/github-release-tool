78b66dae6fdc6ff39b848b4e46feb913631f6cfd
Matt Krueger <mkrueger@rstms.net>
2022-08-19 14:48:52 -0500

    debug gitclean target

--- make.include/common.mk
+++ make.include/common.mk
@@ -28,12 +28,7 @@ help:
 
 .PHONY: gitclean
 gitclean:
-	@if [ -z "$(git status --porcelain)" ]; then \
-	  echo "git status dirty, commit and push first"; \
-	  false;\
-	else \
-	  echo "no uncommitted changes, ready to continue..."; \
-	fi
+	echo $(if $$(git status --porcelain),$(error git status dirty, commit and push first"),$(info no uncommitted changes))
 
 
 testo: gitclean
