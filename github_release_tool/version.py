2569db8d5bf973bf4fd7b40c86199f2aeacd480c
Matt Krueger <mkrueger@rstms.net>
2022-08-19 08:00:22 -0500

    Bump version: 0.1.19 → 0.1.20

--- github_release_tool/version.py
+++ github_release_tool/version.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.19"
+__version__ = "0.1.20"
 __author__ = "Matt Krueger"
 __email__ = "mkrueger@rstms.net"
-__timestamp__ = "2022-08-19 07:59:01-05:00"
+__timestamp__ = "2022-08-19 08:00:22-05:00"
