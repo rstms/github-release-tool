bf725a0366eb78954ef10baba3f33c506b692861
Matt Krueger <mkrueger@rstms.net>
2022-08-19 14:12:17 -0500

    modify gitclean target

--- make.include/common.mk
+++ make.include/common.mk
@@ -26,7 +26,7 @@ help:
 # break with an error if there are uncommited changes
 .PHONY: gitclean
 gitclean:
-	$(if $(shell git status --porcelain),$(error "git status dirty, commit and push first"),git status --short)
+	@$(if $(shell git status --porcelain),$(error "git status dirty, commit and push first"),git status --short)
 
 
 # require user confirmation   example: $(call verify_action,do something destructive)
